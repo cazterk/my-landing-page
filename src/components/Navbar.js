1e004aa681473c4513ea60f8fdaf20cab584cdda
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2021-01-25 16:03:58 +0200

    navbar element

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  window.onload = function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+    const links = document.querySelectorAll(".nav-links li");
+
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("open");
+    });
+  };
+
+  return (
+    <>
+      <div className="nav-section">
+        <nav>
+          <div className="hamburger">
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
