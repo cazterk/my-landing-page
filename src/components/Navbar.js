b7eac554afcc90427942a777eacd91e60a3849b4
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2021-01-26 15:05:16 +0200

    added more navbar elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
   window.onload = function () {
+    const btnhamburger = document.querySelector("#btn-hamburger");
     const hamburger = document.querySelector(".hamburger");
     const navLinks = document.querySelector(".nav-links");
     const links = document.querySelectorAll(".nav-links li");
@@ -13,16 +15,31 @@ function Navbar() {
         link.classList.toggle("fade");
       });
     });
+
+    btnhamburger.addEventListener("click", function () {
+      console.log("hamburger");
+
+      if (btnhamburger.classList.contains("open")) {
+        btnhamburger.classList.remove("open");
+      } else {
+        btnhamburger.classList.add("open");
+      }
+    });
   };
 
   return (
     <>
       <div className="nav-section">
         <nav>
-          <div className="hamburger">
-            <div className="line"></div>
-            <div className="line"></div>
-            <div className="line"></div>
+          <Link to="/" className="navbar-logo">
+            {/* < className="navbar-icon" /> */}
+            <span className="logo-left">caz</span>
+            <spam className="logo-right">terk</spam>
+          </Link>
+          <div id="btn-hamburger" className="hamburger">
+            <div className="line-1"></div>
+            <div className="line-2"></div>
+            <div className="line-3"></div>
           </div>
           <ul className="nav-links">
             <li>
