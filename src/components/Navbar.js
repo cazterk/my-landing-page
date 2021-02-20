14e67a988d3e8d33effaaad3113a0c810c907868
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2021-02-20 14:46:38 +0200

    implimented navbar open in react

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link as Scroll } from "react-scroll";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
-  window.onload = function () {
-    const btnhamburger = document.querySelector("#btn-hamburger");
-    const hamburger = document.querySelector(".hamburger");
-    const navLinks = document.querySelector(".nav-links");
-    const links = document.querySelectorAll(".nav-links li");
-    const navleft = document.querySelector("#nav-left");
-
-    hamburger.addEventListener("click", () => {
-      navLinks.classList.toggle("open");
-      links.forEach((link) => {
-        link.classList.toggle("fade");
-      });
-    });
-
-    btnhamburger.addEventListener("click", function () {
-      console.log("hamburger");
-
-      if (btnhamburger.classList.contains("open")) {
-        btnhamburger.classList.remove("open");
-      } else {
-        btnhamburger.classList.add("open");
-      }
-    });
-
-    navleft.addEventListener("click", () => {
-      navLinks.classList.remove("open");
-      console.log("remove");
-
-      btnhamburger.classList.contains("open");
-      btnhamburger.classList.remove("open");
-
-      links.forEach((link) => {
-        link.classList.toggle("fade");
-      });
-    });
-  };
+  const [menuActive, setMenuActive] = useState(false);
 
   return (
     <>
@@ -48,22 +13,33 @@ function Navbar() {
           <Link to="/" className="navbar-logo">
             <img src="./images/logo-caz.svg" alt="" />
           </Link>
-          <div id="btn-hamburger" className="hamburger">
+          <div
+            onClick={() => setMenuActive(!menuActive)}
+            className={`hamburger ${menuActive && "open"}`}
+          >
             <div className="line-1"></div>
             <div className="line-2"></div>
             <div className="line-3"></div>
           </div>
-          <ul className="nav-links" id="nav-left">
-            <li id="nav-left">
-              <Scroll to="features" smooth={true} duration={1000}>
+          <ul className={`nav-links ${menuActive && "open"}`}>
+            <li className={`nav-left ${menuActive && "fade"}`}>
+              <Scroll
+                to="features"
+                smooth={true}
+                duration={1000}
+                onClick={() => setMenuActive(!menuActive)}
+              >
                 <Link>Projects</Link>
               </Scroll>
             </li>
-            {/* <li id="nav-left">
-              <Link>Contact</Link>
-            </li> */}
-            <li id="nav-left">
-              <Scroll to="about" smooth={true} duration={1000}>
+
+            <li className={`nav-left ${menuActive && "fade"}`}>
+              <Scroll
+                to="about"
+                smooth={true}
+                duration={1000}
+                onClick={() => setMenuActive(!menuActive)}
+              >
                 <Link>About</Link>
               </Scroll>
             </li>
